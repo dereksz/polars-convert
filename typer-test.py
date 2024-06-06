4420669e4bda918bb1678dad6eea9c140c6c6101
Derek Slone-Zhen <derek@slone-zhen.com>
2024-06-06 18:30:34 +1000

    `typer` feature request: https://github.com/tiangolo/typer/discussions/851

--- typer-test.py
+++ typer-test.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+# RUS AS: ./typer-test.py in typer-test.py : out t.t : go
+
 import sys
-from typing import Annotated, Optional
+from typing import Annotated, Dict, Optional
 import typer
-from enum import Enum
 from pathlib import Path
+from shutil import copyfile
 
 # See: https://click-shell.readthedocs.io/en/latest/usage.html#factory-method
 import click
@@ -17,74 +19,34 @@ def my_app(ctx):
 
 # Back to normal `typer` code
 
+STATE: Dict[str, Path] = {}
+
 app = typer.Typer(
     no_args_is_help=False,
     pretty_exceptions_short=False,
-    chain=True,
 )
 
-global_options = {"verbose": False}
+@app.command("in")
+def cmd_in(name: Path):
+    STATE["IN"] = name
 
-# For sub-commands, see: https://typer.tiangolo.com/tutorial/subcommands/add-typer/
+@app.command("out")
+def cmd_out(name: Path):
+    STATE["OUT"] = name
 
-@app.command()
-def main(
-    name: str,
-    last_name: Annotated[Optional[str], typer.Argument(help="Last Name")] = None
+@app.command("go")
+def cmd_go():
+    _in = STATE["IN"]
+    _out = STATE["OUT"]
+    copyfile(_in, _out)
     
-    # Other Examples
-    # EnvVar: Annotated[str, typer.Argument(envvar="AWESOME_NAME")] = "default"
-    # Prompt!!: Annotated[str, typer.Option(prompt=True, confirmation_prompt=True, hide_input=True)] - or "Text" instead of True
-    # Adding short names: typer.Option("--user-name", "-n")]
-    # Custom validation: callback=name_callback -> raise typer.BadParameter("msg")
-    # Path:
-        # config: Annotated[
-        #     Path,
-        #     typer.Option(
-        #         exists=True,
-        #         file_okay=True,
-        #         dir_okay=False,
-        #         writable=False,
-        #         readable=True,
-        #         resolve_path=True,
-        #     ),
-        # ],    
-):
-    """Doc-string will be part of help text!!!"""
-    if last_name:
-        print(f"Hello {name} {last_name}")
-    else:
-        print(f"Hello {name}")
-        
-@app.command()
-def not_main(
-    name: str,
-    last_name: Annotated[Optional[str], typer.Argument(help="Last Name")] = None
-    
-    # Other Examples
-    # EnvVar: Annotated[str, typer.Argument(envvar="AWESOME_NAME")] = "default"
-    # Prompt!!: Annotated[str, typer.Option(prompt=True, confirmation_prompt=True, hide_input=True)] - or "Text" instead of True
-    # Adding short names: typer.Option("--user-name", "-n")]
-    # Custom validation: callback=name_callback -> raise typer.BadParameter("msg")
-    # Use an enumeration sub-type to get restricted set - class NeuralNetwork(str, Enum):...
-):
-    """Doc-string will be part of help text!!!"""
-    if last_name:
-        print(f"Hello {name} {last_name}")
-    else:
-        print(f"Hello {name}")
-        
-
 @app.callback(invoke_without_command=True)
-def base(ctx: typer.Context, verbose: bool = False):
+def base(ctx: typer.Context):
     if ctx.invoked_subcommand is None:
         # See: https://click-shell.readthedocs.io/en/latest/usage.html#factory-method
-        shell = make_click_shell(ctx, prompt='my-app > ', intro='Starting my app...')
+        shell = make_click_shell(ctx, prompt=f'{sys.argv[0]} > ', intro='Starting up...')
         shell.cmdloop()
         typer.Exit(0)
-    if verbose:
-        print("Will write verbose output")
-        global_options["verbose"] = True
 
 if __name__ == "__main__":
     # typer.run(main) # When only a single command
