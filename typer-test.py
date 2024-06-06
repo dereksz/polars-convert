2d9096ac1fe8fe57c408d7f2f22db15ade70f5c7
Derek Slone-Zhen <derek@slone-zhen.com>
2024-06-06 18:13:42 +1000

    Multiple commands

--- typer-test.py
+++ typer-test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 from typing import Annotated, Optional
 import typer
 from enum import Enum
@@ -19,6 +20,7 @@ def my_app(ctx):
 app = typer.Typer(
     no_args_is_help=False,
     pretty_exceptions_short=False,
+    chain=True,
 )
 
 global_options = {"verbose": False}
@@ -83,7 +85,26 @@ def base(ctx: typer.Context, verbose: bool = False):
     if verbose:
         print("Will write verbose output")
         global_options["verbose"] = True
-        
+
 if __name__ == "__main__":
-    # typer.run(main) # When only a single command 
-    app() # for multiple commands
+    # typer.run(main) # When only a single command
+    orig_argv = sys.argv
+    separators = [ idx for idx, arg in enumerate(orig_argv) if arg == ":" ]
+    if not separators:
+        app() # dispatch to one of multiple commands
+    else: # hack for dispatching MULTIPLE command from a single command line invocation
+        separators.append(len(orig_argv)) # Adding a "full-back" makes the loop logic easier
+        orig_argv = orig_argv.copy()
+        start_idx = 1
+        for sep_idx in separators:
+            if start_idx == sep_idx: # catch a separator at the end or 2 next to each other
+                raise typer.BadParameter("Bad command separator layout")
+            new_argv = [orig_argv[0]]
+            new_argv.extend(orig_argv[start_idx:sep_idx])
+            sys.argv = new_argv
+            try: # Could try this instead: https://github.com/tiangolo/typer/issues/129
+                app()
+            except SystemExit as ex:
+                if ex.args[0] != 0:
+                    raise
+            start_idx = sep_idx + 1
